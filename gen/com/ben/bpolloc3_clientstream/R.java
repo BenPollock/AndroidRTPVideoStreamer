9705d017809b6e4c1d6ef10dbc26f9b146b9a019
Ben Pollock <B.O.Pollock@gmail.com>
2013-04-04 22:10:52 -0400

    Add Buttons

--- gen/com/ben/bpolloc3_clientstream/R.java
+++ gen/com/ben/bpolloc3_clientstream/R.java
@@ -14,7 +14,11 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int menu_settings=0x7f070000;
+        public static final int buttonPause=0x7f070003;
+        public static final int buttonPlay=0x7f070001;
+        public static final int buttonSetup=0x7f070002;
+        public static final int buttonTeardown=0x7f070000;
+        public static final int menu_settings=0x7f070004;
     }
     public static final class layout {
         public static final int activity_main_client_view=0x7f030000;
